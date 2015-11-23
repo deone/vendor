2167c87e50121274de36c7b5c788e058adb85bfb
Dayo Osikoya <alwaysdeone@gmail.com>
2015-11-23 17:44:35 +0100

    Vendor model and migrations. Enable it in admin.

--- vend/admin.py
+++ vend/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User, Group
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from .models import Vendor
+
+class VendorInline(admin.StackedInline):
+    model = Vendor
+
+class AccountsUserAdmin(UserAdmin):
+    inlines = (VendorInline, )
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+admin.site.register(User, AccountsUserAdmin)
--- vend/models.py
+++ vend/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Vendor(models.Model):
+    user = models.OneToOneField(User)
+    company_name = models.CharField(max_length=50)
