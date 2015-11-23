99fc30c0518a99cfcba43e2b65684c6ed4a036d3
Dayo Osikoya <alwaysdeone@gmail.com>
2015-11-23 15:44:26 +0100

    Add accounts app, url form and template.

--- vendor/urls.py
+++ vendor/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
+from accounts.forms import VendorAuthenticationForm
 from vend import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^accounts/login/$', auth_views.login, {'template_name': 'accounts/login.html', 'authentication_form': VendorAuthenticationForm}, name='login'),
     url(r'^admin/', include(admin.site.urls)),
 ]
