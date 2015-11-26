d8da41ebfa7c5c00869da42fefb6feb38c35e098
Dayo Osikoya <alwaysdeone@gmail.com>
2015-11-26 16:11:38 +0100

    Merge user_settings and settings.

--- vendor/settings.py
+++ vendor/settings.py
@@ -112,4 +112,32 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-from .user_settings import *
+UNIT = 'GHS'
+
+# price choices
+ONE = 1
+TWO = 2
+FIVE = 5
+
+PRICE_CHOICES = (
+    ('', 'Select Price'),
+    (ONE, '1 ' + UNIT),
+    (TWO, '2 ' + UNIT),
+    (FIVE, '5 ' + UNIT),
+)
+
+# quantity choices
+ONE = 1
+TWO = 2
+THREE = 3
+FOUR = 4
+FIVE = 5
+
+QUANTITY_CHOICES = (
+    ('', 'Select Quantity'),
+    (ONE, '1'),
+    (TWO, '2'),
+    (THREE, '3'),
+    (FOUR, '4'),
+    (FIVE, '5'),
+)
