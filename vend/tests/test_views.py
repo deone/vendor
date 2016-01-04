03c844f761540a4b23a0bcf0c93f87a839e2b852
Dayo Osikoya <alwaysdeone@gmail.com>
2016-01-04 13:01:51 +0100

    Update tests.

--- vend/tests/test_views.py
+++ vend/tests/test_views.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.conf import settings
 
-from ..forms import VendForm
+from ..forms import VendStandardVoucherForm, VendInstantVoucherForm
 from ..models import Vendor
 from ..views import index
-from ..helpers import send_api_request
+from ..helpers import send_api_request, get_price_choices
 
 class ViewsTests(TestCase):
 
@@ -17,12 +17,12 @@ class ViewsTests(TestCase):
 
     def test_index_get(self):
         self.c.post(reverse('login'), {'username': 'z@z.com', 'password': '12345'})
-        response = self.c.get(reverse('index'))
+        response = self.c.get(reverse('vend_instant'))
 
         self.assertEqual(response.status_code, 200)
         self.assertTrue('form' in response.context)
-        self.assertTemplateUsed(response, 'vend/index.html')
-        self.assertTrue(isinstance(response.context['form'], VendForm))
+        self.assertTemplateUsed(response, 'vend/vend_instant.html')
+        self.assertTrue(isinstance(response.context['form'], VendInstantVoucherForm))
 
     def test_index_post(self):
         self.c.post(reverse('login'), {'username': 'z@z.com', 'password': '12345'})
@@ -30,24 +30,22 @@ class ViewsTests(TestCase):
         factory = RequestFactory()
         session = SessionMiddleware()
 
-        # If the vouchers table is empty (which should be the case), VendForm
-        # doesn't load the prices. So we are just gonna create some actual vouchers
-        # for this test to work till we're able to do the ideal thing.
+        pin_one = {'pin': '12345678901234'}
+        pin_two = {'pin': '12345678901233'}
 
-        # pin_one = {'pin': '12345678901234'}
-        # pin_two = {'pin': '12345678901233'}
+        send_api_request(settings.VOUCHER_STUB_INSERT_URL, pin_one)
+        send_api_request(settings.VOUCHER_STUB_INSERT_URL, pin_two)
 
-        # send_api_request(settings.VOUCHER_STUB_INSERT_URL, pin_one)
-        # send_api_request(settings.VOUCHER_STUB_INSERT_URL, pin_two)
+        prices = get_price_choices('STD')
 
         vendor = Vendor.objects.create(user=self.user, company_name="Vender Inc.")
-        request = factory.post(reverse('index'), data={'value': 5, 'quantity': 2})
+        request = factory.post(reverse('vend_standard'), data={'value': 5, 'quantity': 2})
         request.user = self.user
 
-        response = index(request)
+        response = index(request, template='vend/vend_standard.html', vend_form=VendStandardVoucherForm, prices=prices)
 
-        # send_api_request(settings.VOUCHER_STUB_DELETE_URL, pin_one)
-        # send_api_request(settings.VOUCHER_STUB_DELETE_URL, pin_two)
+        send_api_request(settings.VOUCHER_STUB_DELETE_URL, pin_one)
+        send_api_request(settings.VOUCHER_STUB_DELETE_URL, pin_two)
 
         self.assertEqual(response['Content-Type'], 'text/csv')
         self.assertNotEqual(response.content, '')
