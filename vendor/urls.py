ab98538c77cb942e50eec49a086fc84e905e2140
Dayo Osikoya <alwaysdeone@gmail.com>
2016-01-04 09:18:23 +0100

    URL and template for /vend/instant/ Use vend_instant and vend_standard as template names.

--- vendor/urls.py
+++ vendor/urls.py
@@ -7,13 +7,18 @@ from vend.forms import VendStandardVoucherForm, VendInstantVoucherForm
 from vend import views
 
 urlpatterns = [
-    # url(r'^$', views.index, name='index'),
     url(r'^$', views.index,
       {
-        'template': 'vend/index.html',
+        'template': 'vend/vend_standard.html',
         'vend_form': VendStandardVoucherForm,
       },
-      name='index'),
+      name='vend_standard'),
+    url(r'^vend/instant/$', views.index,
+      {
+        'template': 'vend/vend_instant.html',
+        'vend_form': VendInstantVoucherForm,
+      },
+      name='vend_instant'),
     url(r'^accounts/logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^accounts/login/$', auth_views.login, {'template_name': 'accounts/login.html', 'authentication_form': VendorAuthenticationForm}, name='login'),
     url(r'^admin/', include(admin.site.urls)),
