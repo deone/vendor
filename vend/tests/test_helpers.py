ea6575e98445863a016d510fbcab5d612a5da8d9
Dayo Osikoya <alwaysdeone@gmail.com>
2016-01-06 19:54:43 +0100

    Test write_vouchers() for instant vouchers.

---
+++ vend/tests/test_helpers.py
@@ -0,0 +1,13 @@
+from django.test import SimpleTestCase
+from django.conf import settings
+
+from ..helpers import write_vouchers
+
+class HelpersTests(SimpleTestCase):
+
+    def test_write_vouchers(self):
+        vouchers = [['0000000375', 'aaa@a.com', '12345'], ['0000000376', 'bbb@a.com', '12345']]
+        _file = settings.VOUCHER_DOWNLOAD_PATH + '/test.csv'
+        f = write_vouchers(vouchers, _file)
+
+        self.assertTrue(f.name.endswith('.csv'))
