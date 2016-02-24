4a4e1c58450be191148699db95eec1ee66796995
Dayo Osikoya <alwaysdeone@gmail.com>
2016-02-24 21:53:47 +0100

    Update production settings.

--- vendor/settings_production.py
+++ vendor/settings_production.py
@@ -1,5 +1,17 @@
-from settings_test import *
+from settings import *
 
 DEBUG = False
 
 IP = '154.117.8.18'
+
+ALLOWED_HOSTS = [IP]
+
+VMS_URL = "http://" + IP + ":8090/vouchers/"
+
+VOUCHER_FETCH_URL = VMS_URL + "fetch/"
+
+VOUCHER_VALUES_URL = VMS_URL + "values/"
+
+VOUCHER_STUB_INSERT_URL = VMS_URL + "insert/"
+
+VOUCHER_STUB_DELETE_URL = VMS_URL + "delete/"
