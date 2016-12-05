1fc3c7662b283b2e66c0b82f42bcf6d24126627a
Oladayo Osikoya <alwaysdeone@gmail.com>
2016-12-05 17:14:35 +0000

    Add voucher fields to Vend model.

--- vend/models.py
+++ vend/models.py
@@ -7,7 +7,22 @@ class Vendor(models.Model):
     company_name = models.CharField(max_length=50)
 
 class Vend(models.Model):
+
+    INSTANT = 'INS'
+    STANDARD = 'STD'
+
+    TYPE_CHOICES = (
+        ('', 'Select Type'),
+        (STANDARD, 'Standard'),
+        (INSTANT, 'Instant'),
+    )
+
     vendor = models.ForeignKey(Vendor)
     phone_number = models.CharField(max_length=10)
-    voucher_id = models.PositiveSmallIntegerField()
+    voucher_id = models.PositiveSmallIntegerField(null=True)
+    voucher_value = models.PositiveSmallIntegerField()
+    voucher_type = models.CharField(max_length=3, choices=TYPE_CHOICES)
     vend_date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return "%s - %s - %s" % (self.vendor.company_name, self.phone_number, str(self.value))
