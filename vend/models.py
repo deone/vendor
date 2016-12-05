14ee260898257d2ad7a7e3d3474bfd92cfe1f8cb
Oladayo Osikoya <alwaysdeone@gmail.com>
2016-12-05 16:48:43 +0000

    Add Vend model.

--- vend/models.py
+++ vend/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Vendor(models.Model):
     user = models.OneToOneField(User)
     company_name = models.CharField(max_length=50)
+
+class Vend(models.Model):
+    vendor = models.ForeignKey(Vendor)
+    phone_number = models.CharField(max_length=10)
+    voucher_id = models.PositiveSmallIntegerField()
+    vend_date = models.DateTimeField(default=timezone.now)
