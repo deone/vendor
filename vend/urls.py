b96d15b5875223652f53410baf80213169b86bbc
Oladayo Osikoya <alwaysdeone@gmail.com>
2016-12-06 20:36:32 +0000

    Separate vend urls.

---
+++ vend/urls.py
@@ -0,0 +1,33 @@
+from django.conf.urls import url
+from django.conf import settings
+
+from . import views
+from utils import get_price_choices
+
+urlpatterns = []
+
+if 'STD' in settings.VOUCHER_TYPES:
+    urlpatterns += [
+      url(r'^$', views.index,
+      {
+        'template': 'vend/vend_standard.html',
+        'prices': get_price_choices('STD'),
+        'voucher_type': 'STD',
+      },
+      name='standard'),
+    ]
+
+if 'INS' in settings.VOUCHER_TYPES:
+    urlpatterns += [
+      url(r'^vend/instant/$', views.index,
+      {
+        'template': 'vend/vend_instant.html',
+        'prices': get_price_choices('INS'),
+        'voucher_type': 'INS',
+      },
+      name='instant'),
+    ]
+
+urlpatterns += [
+    url(r'^report/$', views.report, name='report'),
+]
--- vendor/urls.py
+++ vendor/urls.py
@@ -1,39 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.conf import settings
 
 from accounts.forms import VendorAuthenticationForm
-from vend.forms import VendForm
-from vend import views
-from utils import get_price_choices
 
-urlpatterns = []
-
-if 'STD' in settings.VOUCHER_TYPES:
-    urlpatterns += [
-      url(r'^$', views.index,
-      {
-        'template': 'vend/vend_standard.html',
-        'prices': get_price_choices('STD'),
-        'voucher_type': 'STD',
-      },
-      name='vend_standard'),
-    ]
-
-if 'INS' in settings.VOUCHER_TYPES:
-    urlpatterns += [
-      url(r'^vend/instant/$', views.index,
-      {
-        'template': 'vend/vend_instant.html',
-        'prices': get_price_choices('INS'),
-        'voucher_type': 'INS',
-      },
-      name='vend_instant'),
-    ]
-
-urlpatterns += [
-    url(r'^report/$', views.report, name='report'),
+urlpatterns = [
+    url(r'', include('vend.urls', namespace="vend")),
     url(r'^accounts/logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^accounts/login/$', auth_views.login, {'template_name': 'accounts/login.html', 'authentication_form': VendorAuthenticationForm}, name='login'),
     url(r'^admin/', include(admin.site.urls)),
