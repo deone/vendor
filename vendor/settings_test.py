0171a91b583636d8ac90ed2ab81b8a7eae8294aa
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-05-08 10:30:56 +0000

    Specify appropriate endpoints.

--- vendor/settings_test.py
+++ vendor/settings_test.py
@@ -14,15 +14,6 @@ DATABASES = {
     }
 }
 
-# VMS settings
-VMS_URL = "http://154.117.12.5:8080/vouchers/"
-VOUCHER_GET_URL = VMS_URL + "get/"
-VOUCHER_VALUES_URL = VMS_URL + "values/"
-VOUCHER_REDEEM_URL = VMS_URL + "redeem/"
-VOUCHER_STUB_INSERT_URL = VMS_URL + "insert/"
-VOUCHER_STUB_DELETE_URL = VMS_URL + "delete/"
+VMS_URL = 'http://154.117.12.5:8080/vouchers/'
 
-# Billing settings
-BILLING_URL = "http://" + IP + "/"
-ACCOUNT_RECHARGE_URL = BILLING_URL + "accounts/topup/"
-ACCOUNT_CREATE_URL = BILLING_URL + "accounts/create_test/"
+BILLING_URL = 'http://154.117.12.4/'
