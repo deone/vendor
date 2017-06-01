2c8bda49ec7ef99fea9dc833f6fb42798e620671
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-06-01 15:23:47 +0000

    This was omitted from the previous commit.

--- vendor/settings_production.py
+++ vendor/settings_production.py
@@ -6,9 +6,11 @@ DEBUG = False
 VMS_URL = 'http://154.117.12.5/vouchers/'
 VOUCHER_GET_URL = VMS_URL + 'get'
 VOUCHER_INVALIDATE_URL = VMS_URL + 'invalidate'
-VOUCHER_VALUES_URL = VMS_URL + 'values/'
-VOUCHER_STUB_INSERT_URL = VMS_URL + 'insert/'
-VOUCHER_STUB_DELETE_URL = VMS_URL + 'delete/'
+VOUCHER_VALUES_URL = VMS_URL + 'values'
+VOUCHER_TEST_USER_CREATE_URL = VMS_URL + 'create_test_user'
+VOUCHER_TEST_USER_DELETE_URL = VMS_URL + 'delete_test_user'
+VOUCHER_STUB_INSERT_URL = VMS_URL + 'create_test_voucher'
+VOUCHER_STUB_DELETE_URL = VMS_URL + 'delete_test_voucher'
 
 # Billing settings
 BILLING_URL = 'http://xwf.spectrawireless.com/'
