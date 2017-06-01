f00b6f9be09e857ae5f9c0735c9e68acc5271e13
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-06-01 15:22:43 +0000

    Correct endpoints.

--- vendor/test_settings_test.py
+++
@@ -1,9 +0,0 @@
-from django.test import SimpleTestCase
-
-from settings_test import *
-
-class TestSettingsTest(SimpleTestCase):
-    def test_settings(self):
-        self.assertEqual(DATABASES['default']['NAME'], 'vendor_test')
-        self.assertEqual(VMS_URL, 'http://154.117.12.5:8080/vouchers/')
-        self.assertEqual(BILLING_URL, 'http://154.117.12.4/')
